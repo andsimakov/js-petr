387e9fb58277d20f75c3d30ef10740bd9fd1a3dc
Andrii Simakov <andsimakov@gmail.com>
2023-05-26 15:11:43 +0300

    L33

---
+++ s2/js/script33.js
@@ -0,0 +1,47 @@
+'use strict';
+
+const arr = [2, 13, 26, 8, 10];
+arr.sort(); // [ 10, 13, 2, 26, 8 ] - числа принялись как строки, потому отсортировало как строки
+
+
+function compareNum(a, b) {
+    return a - b;
+}
+arr.sort(compareNum); // [ 2, 8, 10, 13, 26 ] - отсортировало как надо с помощью коллбека
+
+console.log(arr);
+// console.log(arr.length);
+
+// Удаление последнего элемента
+// arr.pop();
+// console.log(arr);
+
+// // Добавление нового последнего элемента
+// arr.push(10);
+// console.log(arr);
+
+// Обычно для перебора массива используют
+// arr.forEach(function(item, i, arr) {
+//     console.log(`${i}: ${item} inside array ${arr}`);
+// });
+
+
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+// }
+
+// Но также можно использовать и это. Здесь можно использовать break и continue
+// for (let value of arr) {
+//     console.log(arr[value]);
+// }
+
+// Сформировать массив из строки
+// const str = prompt('', '');
+// const products = str.split(', ');
+// // console.log(products);
+
+// // Отсортировать элементы массива как строки
+// products.sort();
+
+// // Склеить массив в строку с разделителем
+// console.log(products.join('; '));
